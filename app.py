cc31cab8e7d8ebe58f5933bdbfdf34b7b7e3751c
JakaxKato <wijayagamingpro@gmail.com>
2025-06-08 23:22:27 +0700

    Add nosec in app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ def test_homepage():
     app = create_app()
     client = app.test_client()
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == 200 # nosec B101
